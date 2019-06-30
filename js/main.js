1444195e6da981516f5bc75270130c0b24afb82f
Marko Rapaic <markorapaic@gmail.com>
2019-06-30 23:31:03 +0930

    Updating menu functionality

--- js/main.js
+++ js/main.js
@@ -1,4 +1,41 @@
 ---
 ---
 
-console.log("Smoogipoo");
+$('.hamburger').click(function() {
+    $(this).toggleClass('is-active');
+});
+
+var $menu = $('#smgi-menu').mmenu(
+    {
+        extensions: [
+        'border-full',
+        'effect-menu-fade',
+        'pagedim-black',
+        'shadow-page',
+        'theme-dark',
+        'position-right',
+        ],
+        navbars: [
+            {
+            'position': 'top'
+            }
+        ]
+        // options
+    }
+);
+
+var $icon = $('.mmenu-button');
+var API = $menu.data('mmenu');
+
+$icon.on('click', function() {
+API.open();
+API.close();
+});
+
+API.bind('open:start', function() {
+$icon.addClass('is-active');
+});
+
+API.bind('close:start', function() {
+$icon.removeClass('is-active');
+});
