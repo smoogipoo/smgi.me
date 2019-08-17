9cc6957e8be22c6cef91527af71a38630e148e32
smoogipoo <smoogipoo@smgi.me>
2019-08-17 22:33:31 +0900

    Auto-format js

--- js/main.js
+++ js/main.js
@@ -1,43 +1,38 @@
-$('.hamburger').click(function() {
+$('.hamburger').click(function () {
     $(this).toggleClass('is-active');
 });
 
-var $menu = $('#smgi-menu').mmenu(
-    {
-        extensions: [
+var $menu = $('#smgi-menu').mmenu({
+    extensions: [
         'border-full',
         'effect-menu-fade',
         'pagedim-black',
         'shadow-page',
         'theme-dark',
         'position-right',
-        ],
-        navbars: [
-            {
-            'position': 'top'
-            }
-        ]
-        // options
-    },
-    {
-        offCanvas: {
-            clone: true
-        }
+    ],
+    navbars: [{
+        'position': 'top'
+    }]
+    // options
+}, {
+    offCanvas: {
+        clone: true
     }
-);
+});
 
 var $icon = $('.mmenu-button');
 var API = $menu.data('mmenu');
 
-$icon.on('click', function() {
-API.open();
-API.close();
+$icon.on('click', function () {
+    API.open();
+    API.close();
 });
 
-API.bind('open:start', function() {
-$icon.addClass('is-active');
+API.bind('open:start', function () {
+    $icon.addClass('is-active');
 });
 
-API.bind('close:start', function() {
-$icon.removeClass('is-active');
+API.bind('close:start', function () {
+    $icon.removeClass('is-active');
 });
